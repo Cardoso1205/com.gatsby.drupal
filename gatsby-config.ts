c25fe682d58326b063e35bc2310faabc7fd785ea
Oscar <PC@MacBook-Pro.local>
2024-05-27 16:27:01 -0600

    setup gatsby

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,22 +1,42 @@
 import type { GatsbyConfig } from "gatsby";
+const dotenv = require("dotenv");
+
+// dotenv.config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// });
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `www.gatsby.drupal`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "Drupal",
+        fieldName: "drupal",
+        url: `https://dev-www-gatsby-backend.pantheonsite.io/graphql`,
+        headers: {
+          Authorization: `Basic ${Buffer.from("admin:Cardozo1406@").toString("base64")}`,
+        },
+      },
+    },
+  ],
 };
 
 export default config;
